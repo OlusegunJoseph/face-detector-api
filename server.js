6c2a033659746ddbf2dd69af2408ad8bb7055b46
Olusegun Joseph Kayode <98201568+OlusegunJoseph@users.noreply.github.com>
2023-01-30 15:25:45 -0500

    adding database connections

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-adjacent-97303",
-    port: 5432,
-    user: "Toshiba",
-    password: "",
-    database: "face-detector",
+    host: process.env.DATABASE_URL,
+    SSL: true,
   },
 });
 
