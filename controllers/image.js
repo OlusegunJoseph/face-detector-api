7685a29e59cf84ef6c286b64bab9482c5cd5a29d
Olusegun Joseph Kayode <98201568+OlusegunJoseph@users.noreply.github.com>
2023-01-30 16:28:38 -0500

    removes api key from code base

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require("clarifai");
 
 const app = new Clarifai.App({
-  apiKey: "4420fe444d884154a7beb6bb73bdb060",
+  apiKey: process.env.API_CLARIFAI,
 });
 
 const handleApiCall = (req, res) => {
